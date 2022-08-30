3838fe7bc0eb1801418263ac063a39841f9b98d9
James Johnson <jamesjhnsn1995@gmail.com>
2022-08-29 18:11:41 -0700

    styling login page

--- users/forms.py
+++ users/forms.py
@@ -8,6 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email')
         
+        
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
