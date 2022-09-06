3c832d026c44f019346c790369db4b5454b3f5b2
James Johnson <jamesjhnsn1995@gmail.com>
2022-09-06 14:54:36 -0700

    finished api serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,30 @@
+from dataclasses import fields
 from rest_framework import serializers
 
 from pokemon.models import Pokemon, Type
 
-class PokemonSerializer(serializers.modelSerializer)
+class NestedPokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pokemon
+        fields = ('name',)
+        
+        
+class NestedTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ('type',)
+
+
+
+class PokemonSerializer(serializers.ModelSerializer):
+    type_info = NestedTypeSerializer(many=True, source='types', read_only=True)
+    class Meta:
+        model = Pokemon
+        fields = ('id', 'number', 'type_info', 'name', 'height', 'weight', 'image_front', 'image_back')
+        
+        
+class TypeSerializer(serializers.ModelSerializer):
+    pokemon_info = NestedPokemonSerializer(many=True, source='pokemon', read_only=True)
+    class Meta:
+        model = Type
+        fields = ('id', 'type')
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+from pokemon.models import Pokemon, Type
+from .serializers import PokemonSerializer, TypeSerializer
+
+class PokemonViewSet(viewsets.ModelViewSet):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+    
+    
+class TypeViewSet(viewsets.ModelViewSet):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
